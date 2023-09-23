0d553b8b3a408a8e0c46e1cdbc3e99ce2958db93
yogesh dubey <yogesh.dubey.0804@gmail.com>
2023-09-24 01:32:49 +0530

    updated configl.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
 
   // Set the production url of your site here
   url: 'https://gdsc-bit-raipur.github.io',
-  // Set the /<baseUrl>/ pathname under which your site is served
+  // Set the /<baseUrl>/  pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/The-Academecians',
 
