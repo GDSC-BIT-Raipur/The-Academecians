402b15ecdbc6910dbfa42318eed232239e9ac683
yogesh dubey <yogesh.dubey.0804@gmail.com>
2023-09-24 01:10:47 +0530

    updated config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'GDSC-BIT-Raipur', // Usually your GitHub org/user name.
-  projectName: ' The-Academecians', // Usually your repo name.
+  projectName: 'The-Academecians', // Usually your repo name.
 
   onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
